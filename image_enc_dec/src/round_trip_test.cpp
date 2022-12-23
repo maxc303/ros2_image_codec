b84b78863da34ceb4e07984f751ac55b5faa5028
maxc <ymchen303@hotmail.com>
2022-12-22 23:30:59 -0500

    test find nvenc encoder

--- image_enc_dec/src/round_trip_test.cpp
+++ image_enc_dec/src/round_trip_test.cpp
@@ -1,12 +1,24 @@
+
+// Need to warp ffmpeg headers into extern "C"
+extern "C" {
 #include <libavcodec/avcodec.h>
+#include <libavcodec/codec.h>
 #include <libavutil/imgutils.h>
 #include <libavutil/opt.h>
-
+}
 #include <iostream>
+#include <string>
 int main() {
   const AVCodec *codec;
   AVCodecContext *c = NULL;
 
   AVFrame *frame;
   AVPacket *pkt;
+
+  std::string codec_name = "h264_nvenc";
+  codec = avcodec_find_encoder_by_name(codec_name.c_str());
+  if (!codec) {
+    fprintf(stderr, "Codec '%s' not found\n", codec_name.c_str());
+    exit(1);
+  }
 }
