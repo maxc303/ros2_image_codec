251b52c15a86a95f02856963b59d9c3e42987b8f
maxc <ymchen303@hotmail.com>
2022-12-27 12:37:24 -0500

    testing h264 round trip

--- image_enc_dec/src/round_trip_test.cpp
+++ image_enc_dec/src/round_trip_test.cpp
@@ -76,6 +76,7 @@ static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt) {
 
     printf("saving frame %3d\n", dec_ctx->frame_number);
     fflush(stdout);
+    return;
   }
 }
 
@@ -84,23 +85,33 @@ int main(int argc, char **argv) {
   // Parse test infomration
   po::options_description desc(
       "FFmpeg API encoding and decoding round trip test");
-  desc.add_options()("help", "produce help message")  //
-      ("image_dir", po::value<std::string>(), "input images dir");
+  desc.add_options()("help", "produce help message")               //
+      ("input_dir", po::value<std::string>(), "input images dir")  //
+      ("output_dir", po::value<std::string>(), "output images dir");
 
   po::variables_map boost_args;
   po::store(po::parse_command_line(argc, argv, desc), boost_args);
   po::notify(boost_args);
 
-  if (boost_args.count("help") != 0 || boost_args.count("image_dir") == 0) {
+  if (boost_args.count("help") != 0 || boost_args.count("input_dir") == 0) {
     std::cout << desc << std::endl;
     return 1;
   }
 
-  std::filesystem::path image_dir = boost_args["image_dir"].as<std::string>();
+  std::filesystem::path output_dir;
+  bool save_output = false;
+  if (boost_args.count("output_dir") == 0) {
+    std::cout << "output_dir is not provided. No decoded image will be saved."
+              << std::endl;
+  } else {
+    output_dir = boost_args["output_dir"].as<std::string>();
+    save_output = true;
+  }
+  std::filesystem::path input_dir = boost_args["input_dir"].as<std::string>();
   std::vector<std::filesystem::path> image_paths;
 
   int max_num_images = 20;
-  for (const auto &entry : std::filesystem::directory_iterator(image_dir)) {
+  for (const auto &entry : std::filesystem::directory_iterator(input_dir)) {
     image_paths.push_back(entry.path());
     if (image_paths.size() >= max_num_images) {
       break;
@@ -124,7 +135,7 @@ int main(int argc, char **argv) {
   AVFrame *frame;
   AVPacket *pkt;
 
-  std::string encoder_name = "h264_nvenc";
+  std::string encoder_name = "libx264";
   encoder = avcodec_find_encoder_by_name(encoder_name.c_str());
   if (!encoder) {
     fprintf(stderr, "Codec '%s' not found\n", encoder_name.c_str());
@@ -157,7 +168,7 @@ int main(int argc, char **argv) {
    */
   encoder_context->gop_size = 5;
   encoder_context->max_b_frames = 0;
-  encoder_context->pix_fmt = AV_PIX_FMT_BGR0;
+  encoder_context->pix_fmt = AV_PIX_FMT_YUV420P;
   encoder_context->thread_count = 0;
 
   // Extra settings to enable frame can be retrievd without waiting for more
@@ -166,7 +177,7 @@ int main(int argc, char **argv) {
   if (encoder_name == "h264_nvenc") {
     av_opt_set(encoder_context->priv_data, "zerolatency", "1", 0);
     av_opt_set(encoder_context->priv_data, "delay", "0", 0);
-  } else if (encoder_name == "libx264rgb") {
+  } else if (encoder_name == "libx264") {
     av_opt_set(encoder_context->priv_data, "tune", "zerolatency", 0);
   }
   int ret;
@@ -213,15 +224,11 @@ int main(int argc, char **argv) {
     ret = av_frame_make_writable(frame);
     if (ret < 0) exit(1);
 
-    // Copy frame data from bgr channel
-    cv::Mat ch1, ch2, ch3;
-    std::vector<cv::Mat> channels(3);
-    cv::split(img, channels);
-
+    cv::Mat img_yuv420p;
+    cv::cvtColor(img, img_yuv420p, cv::COLOR_BGR2YUV_I420);
+    // Copy frame data from yuv channel
     int ch_size = img.size().width * img.size().height;
-    std::memcpy(frame->data[0], channels[0].data, ch_size * 3);
-    // std::memcpy(frame->data[1], channels[1].data, ch_size);
-    // std::memcpy(frame->data[2], channels[2].data, ch_size);
+    std::memcpy(frame->data[0], img_yuv420p.data, ch_size * 3 / 2);
 
     // timestamp = index * timebase
     frame->pts = frame_idx;
@@ -269,12 +276,11 @@ int main(int argc, char **argv) {
   AVCodecContext *decoder_context = NULL;
   AVFrame *decoded_frame;
   AVPacket *decode_pkt;
-  // Add Low Delay Flag for low latency application.
-  decoder_context->flags |= AV_CODEC_FLAG_LOW_DELAY;
+
   int eof;
   decode_pkt = av_packet_alloc();
   if (!decode_pkt) exit(1);
-  decoder = avcodec_find_decoder_by_name("h264_cuvid");
+  decoder = avcodec_find_decoder_by_name("h264");
   if (!decoder) {
     fprintf(stderr, "Decoder not found\n");
     exit(1);
@@ -284,12 +290,20 @@ int main(int argc, char **argv) {
     fprintf(stderr, "parser not found\n");
     exit(1);
   }
+
+  // Set the PARSER_FLAG_COMPLETE_FRAMES to get the parsed data before the next
+  // frame arrives. The packet is known to be completed.
+  parser->flags |= PARSER_FLAG_COMPLETE_FRAMES;
   decoder_context = avcodec_alloc_context3(decoder);
   if (!decoder_context) {
     fprintf(stderr, "Could not allocate video codec context\n");
     exit(1);
   }
-  decoder_context->time_base = (AVRational){1, 10};
+
+  // Add Low Delay Flag for low latency application.
+  // https://github.com/FFmpeg/FFmpeg/blob/eeb280f3518a8d7fc2e45f06ac7748f42d8a0000/libavcodec/cuviddec.c#L988
+  decoder_context->flags |= AV_CODEC_FLAG_LOW_DELAY;
+  // decoder_context->time_base = (AVRational){1, 10};
 
   /* open it */
   if (avcodec_open2(decoder_context, decoder, NULL) < 0) {
@@ -303,32 +317,67 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  for (auto &pkt : encoded_pkts) {
-    decode(decoder_context, decoded_frame, pkt);
-    av_packet_free(&pkt);
-  }
-  decode(decoder_context, decoded_frame, NULL);
-
-  // for (const auto &pkt_data : encoded_packet_data) {
-  //   std::cout << "Parsing packet (size= " << pkt_data.size() << std::endl;
-  //   ret = av_parser_parse2(parser, decoder_context, &decode_pkt->data,
-  //                          &decode_pkt->size, pkt_data.data(),
-  //                          pkt_data.size(), AV_NOPTS_VALUE, AV_NOPTS_VALUE,
-  //                          0);
-
-  //   std::cout << "Parse result " << av_err2str(ret) << std::endl;
-
-  //   if (ret < 0) {
-  //     fprintf(stderr, "Error while parsing\n");
-  //     exit(1);
-  //   }
-
-  //   if (pkt->size)
-  //     decode(decoder_context, decoded_frame, decode_pkt);
-  //   else
-  //     break;
+  // /*
+  // Use Saved Packet
+  // */
+  // for (auto &pkt : encoded_pkts) {
+  //   decode(decoder_context, decoded_frame, pkt);
+  //   av_packet_free(&pkt);
   // }
+  // decode(decoder_context, decoded_frame, NULL);
 
+  std::vector<cv::Mat> decoded_frames;
+
+  int pkt_idx = 0;
+
+  for (auto &pkt_data : encoded_packet_data) {
+    std::cout << "Parsing packet (size= " << pkt_data.size() << std::endl;
+
+    ret = av_parser_parse2(parser, decoder_context, &decode_pkt->data,
+                           &decode_pkt->size, pkt_data.data(), pkt_data.size(),
+                           AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+
+    std::cout << "Parse result 0 " << av_err2str(ret) << std::endl;
+
+    if (decode_pkt->size) {
+      std::cout << "Parsed a packet" << std::endl;
+    }
+    if (ret < 0) {
+      fprintf(stderr, "Error while parsing\n");
+      exit(1);
+    }
+
+    // if (decode_pkt->size) decode(decoder_context, decoded_frame, decode_pkt);
+    if (decode_pkt->size) {
+      decode(decoder_context, decoded_frame, decode_pkt);
+
+      cv::Mat image(decoded_frame->height * 3 / 2, decoded_frame->width,
+                    CV_8UC1);
+      int y_channel_size = decoded_frame->width * decoded_frame->height;
+      int uv_channel_size = decoded_frame->width * decoded_frame->height / 4;
+      std::memcpy(image.data, decoded_frame->data[0], y_channel_size);
+      std::memcpy(image.data + y_channel_size, decoded_frame->data[1],
+                  uv_channel_size);
+      std::memcpy(image.data + y_channel_size + uv_channel_size,
+                  decoded_frame->data[2], uv_channel_size);
+
+      cv::Mat image_bgr(decoded_frame->height, decoded_frame->width, CV_8UC3);
+      cv::cvtColor(image, image_bgr, cv::COLOR_YUV420p2BGR, 3);
+
+      if (save_output) {
+        auto output_path = output_dir / image_paths[pkt_idx].filename();
+        cv::imwrite(output_path, image_bgr);
+        std::cout << "Saved to output_path << " << output_path << std::endl;
+      }
+    } else {
+      std::cout << "Missing frames for decoding " << av_err2str(ret)
+                << std::endl;
+      break;
+    }
+    pkt_idx++;
+  }
+
+  av_parser_close(parser);
   avcodec_free_context(&decoder_context);
   av_frame_free(&decoded_frame);
   av_packet_free(&decode_pkt);
