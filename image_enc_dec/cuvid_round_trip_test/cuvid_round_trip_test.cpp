51dbaf789935c8353cd98453bdd42ce32463290a
maxc <ymchen303@hotmail.com>
2022-12-30 23:41:58 -0500

    link SDK

---
+++ image_enc_dec/cuvid_round_trip_test/cuvid_round_trip_test.cpp
@@ -0,0 +1,16 @@
+
+// Need to warp ffmpeg headers into extern "C"
+#include "NvEncoder/NvEncoderCuda.h"
+
+// #include <spdlog/cfg/env.h>
+// #include <spdlog/spdlog.h>
+
+// #include <boost/program_options.hpp>
+// #include <filesystem>
+// #include <iostream>
+// #include <opencv4/opencv2/opencv.hpp>
+// #include <opencv4/opencv2/quality.hpp>
+// #include <string>
+
+// namespace po = boost::program_options;
+int main(int argc, char** argv) { return 0; }
