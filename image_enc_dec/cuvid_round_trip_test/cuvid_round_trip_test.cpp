04475eba4db1c8b437dee794db1280e04f311189
maxc <ymchen303@hotmail.com>
2022-12-31 01:08:19 -0500

    checking linkage issue

--- image_enc_dec/cuvid_round_trip_test/cuvid_round_trip_test.cpp
+++ image_enc_dec/cuvid_round_trip_test/cuvid_round_trip_test.cpp
@@ -1,11 +1,12 @@
 
 // Need to warp ffmpeg headers into extern "C"
+#include <spdlog/cfg/env.h>
+#include <spdlog/spdlog.h>
+
+#include <boost/program_options.hpp>
+
 #include "NvEncoder/NvEncoderCuda.h"
-
-// #include <spdlog/cfg/env.h>
-// #include <spdlog/spdlog.h>
-
-// #include <boost/program_options.hpp>
+#include "NvEncoderCLIOptions.h"
 // #include <filesystem>
 // #include <iostream>
 // #include <opencv4/opencv2/opencv.hpp>
@@ -13,4 +14,19 @@
 // #include <string>
 
 // namespace po = boost::program_options;
-int main(int argc, char** argv) { return 0; }
+int main(int argc, char** argv) {
+  CUcontext cuContext = NULL;
+  CUdevice cuDevice = 0;
+  int iGpu = 0;
+
+  // cuDeviceGet(&cuDevice, iGpu);
+  // char szDeviceName[80];
+  // ck(cuDeviceGetName(szDeviceName, sizeof(szDeviceName), cuDevice));
+  // std::cout << "GPU in use: " << szDeviceName << std::endl;
+  // ck(cuCtxCreate(&cuContext, 0, cuDevice));
+  // NV_ENC_BUFFER_FORMAT eFormat = NV_ENC_BUFFER_FORMAT_IYUV;
+
+  // NvEncoderCuda enc(cuContext, 1600, 900, eFormat, 0);
+
+  return 0;
+}
